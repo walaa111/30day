a498394b62162b985aca753ad0fe104f37cce4ad
walaa111 <walaagamil18@gmail.com>
2017-12-14 16:15:28 +0200

    l

--- series_app_ads/www/js/index.js
+++ series_app_ads/www/js/index.js
@@ -67,8 +67,8 @@ function initAd(){
                     interstitial: 'ca-app-pub-xxxxxxxxxxx/xxxxxxxxxxx'	//PUT ADMOB ADCODE HERE 
                 },
                 android : {
-                    banner: 'ca-app-pub-7251676025279948/4027309960',		//PUT ADMOB ADCODE HERE 
-                    interstitial: 'ca-app-pub-7251676025279948/8792551488'	//PUT ADMOB ADCODE HERE 
+                    banner: 'ca-app-pub-7251676025279948/8172824132',		//PUT ADMOB ADCODE HERE 
+                    interstitial: 'ca-app-pub-7251676025279948/1886617386'	//PUT ADMOB ADCODE HERE 
                 }
             };
             var admobid = ( /(android)/i.test(navigator.userAgent) ) ? ad_units.android : ad_units.ios;
